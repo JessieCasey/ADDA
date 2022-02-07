e1c3a703cecc74c85a21c121056ff32911ca6d73
JessieCasey <kenysglobal@gmail.com>
2022-02-07 19:53:32 +0200

    Changes in user controller

--- src/main/java/com/adda/controller/UserController.java
+++ src/main/java/com/adda/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+//Controller of users
 @RestController
 @RequestMapping("/user")
 public class UserController {
