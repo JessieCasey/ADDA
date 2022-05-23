b5282b0c36beb26f4aa1b3250129d2742ddcb838
JessieCasey <kenysglobal@gmail.com>
2022-05-23 22:52:33 +0200

    Adda final stage

--- src/main/java/com/adda/controller/UserController.java
+++ src/main/java/com/adda/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("api/user")
 public class UserController {
