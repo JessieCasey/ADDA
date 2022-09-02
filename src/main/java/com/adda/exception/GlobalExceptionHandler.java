6234968f3971a81b48b227b5e9c6d8a9e79b349d
JessieCasey <kenysglobal@gmail.com>
2022-09-02 14:00:51 +0200

    Implementing Loggers to the Controllers and GlobalExceptionHandler

---
+++ src/main/java/com/adda/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.adda.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<?> handleMethodArgumentNotValid(UserNotFoundException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<?> handleNullEntityReferenceException(AdvertisementNotFoundException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<?> handleEntityNotFoundException(UserAlreadyExistException ex) {
+        return ResponseEntity.notFound().build();
+    }
+
+}
