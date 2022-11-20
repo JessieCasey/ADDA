51af04bc2b3863d1bfd616d8851ed94bad3a4869
JessieCasey <kenysglobal@gmail.com>
2022-11-20 17:03:58 +0200

    FlyWay migration implementation

---
+++ src/main/resources/db/migration/V2__INSERT_ROLES_AND_CATEGORIES.sql
@@ -0,0 +1,9 @@
+insert into categories (category_name) values ('Car');
+insert into categories (category_name) values ('Electronics');
+insert into categories (category_name) values ('Beauty');
+insert into categories (category_name) values ('Service');
+
+insert into roles (name) values ('ROLE_USER');
+insert into roles (name) values ('ROLE_MODERATOR');
+insert into roles (name) values ('ROLE_ADMIN');
+insert into roles (name) values ('ROLE_BANNED');
